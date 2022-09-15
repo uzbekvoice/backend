ab59b516d10aea58b600419761e463a1315cb514
Diyor Pardaev <80511099+diyorpardaev@users.noreply.github.com>
2022-09-15 15:37:39 +0900

    writing-id-text-to-models

--- src/apps/sentence/models.py
+++ src/apps/sentence/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 # Create your models here.
 #lets start it
+class Sentence(models.Model):
+    id=models.AutoField(primary_key=True)
+    text=models.CharField(max_length=500)
+    
