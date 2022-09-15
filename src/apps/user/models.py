736aa0ccb2471053b1a930af6f6e412ab413a789
Azizbek <azizbekgulomov>
2022-09-15 19:43:41 +0500

    'added-user-model'

--- src/apps/user/models.py
+++ src/apps/user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 
+class User(models.Model):
+    full_name = models.CharField(max_length=200)
+    td_id = models.IntegerField()
+    gender = models.IntegerField()
+    year_of_birth = models.IntegerField()
+    accent_region = models.IntegerField()
+    native_language = models.IntegerField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    
+    
+    def __str__(self):
+        return self.full_name
+    
+    
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
