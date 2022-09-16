273501828606b7da33e54d911d5bb4bfe3912e8c
Azizbek <azizbekgulomov>
2022-09-16 17:54:32 +0500

    'add-gender-foo-model'

--- src/apps/user/models.py
+++ src/apps/user/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
+class Foo(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
 
 class User(models.Model):
     full_name = models.CharField(max_length=200)
     td_id = models.IntegerField()
-    gender = models.IntegerField()
+    gender = models.ForeignKey(Foo, on_delete=models.CASCADE)
     year_of_birth = models.IntegerField()
     accent_region = models.IntegerField()
     native_language = models.IntegerField()
