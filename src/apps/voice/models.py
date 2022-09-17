8949ad05b96899533e55a34b18e2b0df62abece0
Diyor Pardaev <80511099+diyorpardaev@users.noreply.github.com>
2022-09-18 00:48:09 +0900

    Adding-models

--- src/apps/voice/models.py
+++ src/apps/voice/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from backend.src.apps.user.models import User
 
 # Create your models here.
+class VoiceChecker(models.Model):
+    author=models.ForeignKey(User)
+    voice=models.OneToOneField(Voice)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_ad=models.DateTimeField(auto_now=)
+
+
+    def __str__(self):
+        return self.voice
