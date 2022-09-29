a481234db87745abb4371a61e82b58f3b6810aab
Diyor Pardaev <80511099+diyorpardaev@users.noreply.github.com>
2022-09-29 11:53:15 +0900

    models_created

--- src/apps/voice/models.py
+++ src/apps/voice/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from backend.src.apps.user.models import User
+
 
 # Create your models here.
 class VoiceChecker(models.Model):
-    author=models.ForeignKey(User)
-    voice=models.OneToOneField(Voice)
+    #author=models.ForeignKey(User)
+    #voice=models.OneToOneField(Voice)
     created_at=models.DateTimeField(auto_now_add=True)
-    updated_ad=models.DateTimeField(auto_now=)
+    updated_ad=models.DateTimeField(auto_now=True)
 
 
-    def __str__(self):
-        return self.voice
+    
